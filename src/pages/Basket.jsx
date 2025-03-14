3f2fb54c89b584563d122716b71991ccf3a08650
Jefferston <dima.dima.989@gmail.com>
2025-03-14 20:45:26 +0100

    selectedItemFromFridge

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -194,10 +194,12 @@ export default function BasketPage({ navigation }) {
   };
 
   const [selectedItemId, setSelectedItemId] = useState(null);
+  const [selectedItemFromFridge, setSelectedItemFromFridge] = useState(null);
   const [isInfoModalVisible, setIsInfoModalVisible] = useState(false);
 
-  const handleItemPress = (id) => {
+  const handleItemPress = (id, isFromFridge) => {
     setSelectedItemId(id);
+    setSelectedItemFromFridge(isFromFridge);
     setIsInfoModalVisible(true);
   };
 
@@ -237,6 +239,7 @@ export default function BasketPage({ navigation }) {
             isVisible={isInfoModalVisible} 
             onClose={() => setIsInfoModalVisible(false)} 
             itemId={selectedItemId} 
+            itemType={selectedItemFromFridge}
           />
 
         </View>
