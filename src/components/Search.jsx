904909aa6303beccb787cb5dfc3d058c6f896b8a
Jefferston <dima.dima.989@gmail.com>
2025-07-14 12:40:49 +0200

    also clear bytton was added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { StyleSheet, TextInput } from "react-native";
-// import Entypo from 'react-native-vector-icons/Entypo';
+import { StyleSheet, TextInput, View, Pressable } from "react-native";
+import Entypo from 'react-native-vector-icons/Entypo';
 
-import { MainFont } from '../../assets/Styles/styleVariables';
+import { greyTextColor, greyTextColor2, MainFont } from '../../assets/Styles/styleVariables';
 import { useFonts } from 'expo-font';
 
 export default function SearchInput({ placeholder, query, onChangeText }) {
@@ -11,13 +11,32 @@ export default function SearchInput({ placeholder, query, onChangeText }) {
         'Inter': require('../../assets/fonts/Inter/Inter_18pt-Regular.ttf'),
       });
 
-    // const searchQuery = (searchQuery !== null && typeof searchQuery !== 'object' && searchQuery !== undefined) ? searchQuery : "";
-
     return (         
-        <TextInput style={styles.searchInput} placeholder={placeholder} value={query} onChangeText={(text) => onChangeText(text)} placeholderTextColor={'#9e9e9e'} ></TextInput>
+        // <TextInput style={styles.searchInput} placeholder={placeholder} value={query} 
+        // onChangeText={(text) => onChangeText(text)} placeholderTextColor={'#9e9e9e'} ></TextInput>
+
+        <View style={styles.container}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder={placeholder}
+            value={query}
+            onChangeText={onChangeText}
+            placeholderTextColor={'#9e9e9e'}
+          />
+          {query.length > 0 && (
+            <Pressable style={styles.clearButton} onPress={() => onChangeText('')}>
+              <Entypo name="cross" size={28} color={greyTextColor} />
+            </Pressable>
+          )}
+        </View>
     )}
 
 const styles = StyleSheet.create({
+    container: {
+      position: "relative",
+      width: '100%',
+      alignSelf: 'center',
+    },
     searchInput: {
         // borderColor: '#C0C0C0',
         // borderWidth: 1,
@@ -37,5 +56,11 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.4,
         shadowRadius: 2,
         elevation: 2, 
+      },
+      clearButton: {
+        position: "absolute",
+        right: 10,
+        top: 12,
+        padding: 4,
       },
 })
